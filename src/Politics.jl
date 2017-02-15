5523acd8fb3468c0b1ff305c4098015cfe81a625
Jack Cook <hello@jackcook.nyc>
2017-02-15 18:12:16 -0500

    Initial test retrieving politician data

--- src/Politics.jl
+++ src/Politics.jl
@@ -1,5 +1,10 @@
 module Politics
 
-# package code goes here
+import Requests: get
+using Requests
 
-end # module
+url = "https://www.googleapis.com/civicinfo/v2/representatives"
+query = Dict("address" => "10028", "key" => "AIzaSyBKZFDfEfsZp6ZX-7N1cBWxza-DL7MnOgc")
+res = get(url; query = query)
+
+end
