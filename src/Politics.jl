465c0e55dbac9da35a77fc6d2b453fd6456cdb78
Jack Cook <hello@jackcook.nyc>
2017-02-22 09:45:42 +0100

    Added an error message if an API key isn't passed

--- src/Politics.jl
+++ src/Politics.jl
@@ -7,16 +7,20 @@ using Requests
 include("consts.jl")
 include("politicians.jl")
 
-api_key = ARGS[1]
-conn = mysql_connect("localhost", "root", "cityhub", "cityhub")
+if length(ARGS) > 0
+  api_key = ARGS[1]
+  conn = mysql_connect("localhost", "root", "cityhub", "cityhub")
 
-progress = Progress(length(NYC_ZIPCODES), 1, "Retrieving politicians...", 32)
+  progress = Progress(length(NYC_ZIPCODES), 1, "Retrieving politicians...", 32)
 
-for zipcode in NYC_ZIPCODES
-    get_politicians(api_key, conn, zipcode)
-    next!(progress)
+  for zipcode in NYC_ZIPCODES
+      get_politicians(api_key, conn, zipcode)
+      next!(progress)
+  end
+
+  mysql_disconnect(conn)
+else
+  println("A key for the Google Civic Information API must be passed as an argument in order to retrieve politician data.")
 end
-
-mysql_disconnect(conn)
 
 end
